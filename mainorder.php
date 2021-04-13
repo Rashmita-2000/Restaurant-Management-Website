10d64cfe74a5d677d375e8a0429f8b825397497b
Rashmita-2000 <67673406+Rashmita-2000@users.noreply.github.com>
2021-04-13 11:16:32 +0530

    Delete mainorder.php

--- mainorder.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('database.php');
-	include('function.php');
- // Using database connection file here
- if(isset($_POST["proceedtopay"]))  
- { 
-     redirect('mainorder.php')
- }
- ?>
